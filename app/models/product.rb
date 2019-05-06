dc7dea4c09037b1753e82100f103af70bbd25027
shotasan <wondermoai@gmail.com>
2019-05-06 19:37:46 +0900

    Feat: Product,Cart,line_itemを関連付け

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_bu_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true, length: { minimum: 10, too_short: 'は最低10文字以上入力してください' }
@@ -6,4 +10,15 @@ class Product < ApplicationRecord
       with: %r{\.(gif|jpg|png)\z}i,
       message: 'はGIF,JPG,PNG画像のURLでなければなりません'
   }
+
+  private
+
+  def ensure_not_referenced_bu_any_line_item
+    if line_items.empty?
+      true
+    else
+      errors.add(:base, '品目が存在します')
+      false
+    end
+  end
 end
