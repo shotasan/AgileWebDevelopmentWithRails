6264aee1d3e5307d44281cd3278f3a0a9673c718
shotasan <wondermoai@gmail.com>
2019-05-07 12:07:14 +0900

    Feat: store/indexへの訪問回数を表示する機能を実装

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    if session[:counter].nil?
+      session[:counter] = 1
+      @counter = session[:counter]
+    else
+      session[:counter] += 1
+      @counter = session[:counter]
+    end
   end
 end
