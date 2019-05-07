1759be77d0bd500cb241f2c779b7534e2f4f6ca1
shotasan <wondermoai@gmail.com>
2019-05-07 13:16:46 +0900

    Feat: カートに入れた商品の数量が表示される機能を実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_032409) do
+ActiveRecord::Schema.define(version: 2019_05_07_035056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
