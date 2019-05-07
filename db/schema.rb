7cf645762e56d0b4863057b36566ef4083401b23
shotasan <wondermoai@gmail.com>
2019-05-07 12:25:20 +0900

    Feat: line_itemsテーブルにquantityカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_06_102749) do
+ActiveRecord::Schema.define(version: 2019_05_07_032409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_05_06_102749) do
     t.integer "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
   end
 
   create_table "products", force: :cascade do |t|
