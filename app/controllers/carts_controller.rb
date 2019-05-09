4c0a0d87e0ba02e83a2e7b7fbbfd63bbe2343785
shotasan <wondermoai@gmail.com>
2019-05-09 17:12:27 +0900

    Fix: カートを空にした際に発生するエラーを修正

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -65,10 +65,11 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
+    @cart = current_cart
     @cart.destroy
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'カートは現在空です' }
+      format.html { redirect_to store_url }
       format.json { head :no_content }
     end
   end
