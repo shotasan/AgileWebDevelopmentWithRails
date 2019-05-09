f16f82b71c3586016a0db3232ec52aba659d5819
shotasan <wondermoai@gmail.com>
2019-05-09 17:13:31 +0900

    Fix: cart_controllerのbefore actionを修正

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,5 @@
 class CartsController < ApplicationController
-  before_action :set_cart, only: [:edit, :update, :destroy]
+  before_action :set_cart, only: [:edit, :update]
 
   # GET /carts
   # GET /carts.json
