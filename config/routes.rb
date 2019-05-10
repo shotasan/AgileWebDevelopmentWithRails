efd03f1354edefd6698e299f2ebc233f4c9da946
shotasan <wondermoai@gmail.com>
2019-05-10 22:44:23 +0900

    Feat: 管理機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'admin/index'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  resources :users
   root 'store#index', as: 'store'
+  get 'admin/index'
+  get 'store/index'
+  controller :sessions do
+    get 'login', action: :new
+    post 'login', action: :create
+    delete 'logout', action: :destroy
+  end
+  resources :users
   resources :orders
   resources :line_items
   resources :carts
-  get 'store/index'
   resources :products do
     get :who_bought, on: :member
   end
