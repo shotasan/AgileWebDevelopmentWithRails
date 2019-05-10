61de47f2dbf0dae22367610ebee7dbc6f0a4f221
shotasan <wondermoai@gmail.com>
2019-05-11 01:33:31 +0900

    Feat: 管理者ユーザーが0にならないようにUserモデルにフィルターを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  after_destroy :ensure_an_admin_remains
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  private
+
+  def ensure_an_admin_remains
+    if User.count.zero?
+      raise '最後のユーザは削除できません'
+    end
+  end
 end
